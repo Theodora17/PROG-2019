0c7a9d993690d0e72e722d8885e6c2c3e3236e8e
Theodora17 <34063081+Theodora17@users.noreply.github.com>
2019-09-05 10:17:55 +0000

    max_int

--- Assignment 5 - Algorithms and git/max_int.py
+++ Assignment 5 - Algorithms and git/max_int.py
@@ -2,3 +2,14 @@
 # The first number will be the maximum, because it's the only number
 # We check if the second number is the same size or bigger, if so, that number becomes the biggest
 # we continue like this until the user inputs a negative number
+
+num_int = int(input("Input a number: "))    # Do not change this line
+# Fill in the missing code
+max_int = 0
+
+while num_int >= 0:
+    if num_int >= max_int:
+        max_int = num_int
+    num_int = int(input("Input a number: "))
+
+print("The maximum is", max_int)    # Do not change this line
