541fb0c05bb8459224663f2daafc7b3d45b4b3a6
Theodora17 <34063081+Theodora17@users.noreply.github.com>
2019-09-05 09:57:42 +0000

    Algorithm written for max_int

---
+++ Assignment 5 - Algorithms and git/max_int.py
@@ -0,0 +1,4 @@
+#Algorithm
+# The first number will be the maximum, because it's the only number
+# We check if the second number is the same size or bigger, if so, that number becomes the biggest
+# we continue like this until the user inputs a negative number
